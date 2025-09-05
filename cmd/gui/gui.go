9b5033ffcae24df25b679ac9d23cb0e02156a1d6
hazardous-sun <141582502+hazardous-sun@users.noreply.github.com>
2025-09-04 21:49:28 -0300

    feat(cmd/gui): started to implement the GUI

---
+++ cmd/gui/gui.go
@@ -0,0 +1,37 @@
+package gui
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type GUI struct {
+	window fyne.Window
+}
+
+func NewGUI() *GUI {
+	a := app.New()
+	w := a.NewWindow("Fyne UI")
+	w.Resize(fyne.NewSize(800, 600))
+	w.SetFixedSize(true)
+
+	addBtn := widget.NewButton("Add", func() {
+		fmt.Println("Add")
+	})
+
+	listBtn := widget.NewButton("List", func() {
+		fmt.Println("List")
+	})
+
+	ctr := container.NewHBox(addBtn, listBtn)
+	w.SetContent(ctr)
+	return &GUI{w}
+}
+
+func (g *GUI) Start() {
+	g.window.ShowAndRun()
+}
