db7957549ad3da3dcb9511ed127ece8d9e8a84f9
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-09-07 19:36:44 -0300

    refactor(gui): adjust width for description field in list view

--- cmd/gui/listGUI.go
+++ cmd/gui/listGUI.go
@@ -83,7 +83,7 @@ func makeListContent(g *GUI) fyne.CanvasObject {
 	table.SetColumnWidth(3, 100)
 	table.SetColumnWidth(4, 120)
 	table.SetColumnWidth(5, 200)
-	table.SetColumnWidth(6, 200)
+	table.SetColumnWidth(6, 400)
 
 	table.ShowHeaderColumn = false
 
