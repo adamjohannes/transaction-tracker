7031432f5d4656280ed5d224c785a635d6c2714f
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-09-21 19:08:19 -0300

    feat: add app_logs table

--- backend/doc/postgres/initialize_db.sql
+++ backend/doc/postgres/initialize_db.sql
@@ -1,3 +1,12 @@
+CREATE TABLE public.app_logs
+(
+    id         bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
+    timestamp  timestamp with time zone            NOT NULL DEFAULT now(),
+    level      character varying                   NOT NULL,
+    message    text                                NOT NULL,
+    attributes jsonb,
+    CONSTRAINT app_logs_pkey PRIMARY KEY (id)
+);
 CREATE TABLE public.currencies
 (
     code character varying NOT NULL,
@@ -43,9 +52,9 @@ CREATE TABLE public.transactions
     type         bigint                              NOT NULL,
     essential    boolean                             NOT NULL,
     CONSTRAINT transactions_pkey PRIMARY KEY (id),
-    CONSTRAINT transactions_type_fkey FOREIGN KEY (type) REFERENCES public.transaction_types (id),
-    CONSTRAINT transactions_status_fkey FOREIGN KEY (status) REFERENCES public.transaction_status (id),
-    CONSTRAINT transactions_sub_category_fkey FOREIGN KEY (sub_category) REFERENCES public.transaction_sub_categories (id),
+    CONSTRAINT transactions_currency_fkey FOREIGN KEY (currency) REFERENCES public.currencies (code),
     CONSTRAINT transactions_category_fkey FOREIGN KEY (category) REFERENCES public.transaction_categories (id),
-    CONSTRAINT transactions_currency_fkey FOREIGN KEY (currency) REFERENCES public.currencies (code)
+    CONSTRAINT transactions_sub_category_fkey FOREIGN KEY (sub_category) REFERENCES public.transaction_sub_categories (id),
+    CONSTRAINT transactions_status_fkey FOREIGN KEY (status) REFERENCES public.transaction_status (id),
+    CONSTRAINT transactions_type_fkey FOREIGN KEY (type) REFERENCES public.transaction_types (id)
 );
